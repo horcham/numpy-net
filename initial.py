0b95c4d4dde97aed7ca2e5c81f44cdd87a3eb79a
horcham <690936541@qq.com>
2019-02-16 17:45:44 +0800

    numpynet v1.3

--- initial.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-def UniformInit(shape, low=-1, high=1):
-	return np.random.uniform(low, high, shape)
-
-def NormalInit(shape, loc=0, scale=1.0):
-	return np.random.normal(loc, scale, shape)
-
